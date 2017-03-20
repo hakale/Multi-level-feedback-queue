bf68586e7d644863a519eaab5c129779e4b49a06
yokin <yokinzhang@gmail.com>
2017-03-20 22:51:33 +0800

    add  card process function * add pause and start function to process card

--- app/src/renderer/components/mainDesk.vue
+++ app/src/renderer/components/mainDesk.vue
@@ -1,24 +1,29 @@
 <template>
   <div class="bottom">
     <div class="thirdQueue">
-      <Row
-        is="my-card"
-        v-model="thirdQueueCards"
-        v-for="msg3 in thirdQueueCards"
-        v-bind:message="msg3"
-      >
-      </Row>
+      <transition-group name="slide">
+        <Row
+          is="my-card"
+          v-model="thirdQueueCards"
+          v-for="msg3 in thirdQueueCardsCache"
+          v-bind:message="msg3"
+          v-bind:key="msg3['index']"
+        >
+        </Row>
+      </transition-group>
     </div>
     <div class="secondQueue">
-      <Row
-        is="my-card"
-        v-model="secondQueueCards"
-        v-for="msg2 in secondQueueCards"
-        v-bind:message="msg2"
-      >
-      </Row>
+      <transition-group name="slide">
+        <Row
+          is="my-card"
+          v-model="secondQueueCards"
+          v-for="msg2 in secondQueueCardsCache"
+          v-bind:message="msg2"
+          v-bind:key="msg2['index']"
+        >
+        </Row>
+      </transition-group>
     </div>
-
     <div class="firstQueue">
       <transition-group name="slide">
         <Row
@@ -26,10 +31,35 @@
           v-model="firstQueueCards"
           v-for="msg1 in firstQueueCardsCache"
           v-bind:message="msg1"
-          v-bind:key="msg1['time']"
+          v-bind:key="msg1['index']"
         >
         </Row>
       </transition-group>
+    </div>
+    <div class="buttonsFirstRow">
+      <Button-group shape="circle" size="small">
+        <i-button type="success">
+          <Icon type="chevron-left"></Icon>
+          减速
+        </i-button>
+        <i-button @click.native="pauseQueue" type="success">
+          暂停
+        </i-button>
+        <i-button type="success">
+          加速
+          <Icon type="chevron-right"></Icon>
+        </i-button>
+      </Button-group>
+    </div>
+    <div class="buttonsSecondRow">
+      <i-button @click.native="startQueue" type="primary" size="small" shape="circle" long>
+        开始
+      </i-button>
+    </div>
+    <div class="buttonsThirdRow">
+      <i-button type="error" size="small" shape="circle" long>
+        结束
+      </i-button>
     </div>
     <draggable class="selected" v-model="selectedCards" :options="dragOptions" :move="onMove" >
       <Row>
@@ -63,27 +93,38 @@
         waitingCards: [
           {
             id: 'progress1',
+            index: 1,
             totaltime: 2,
             time: 2
           },
           {
             id: 'progress2',
+            index: 2,
             totaltime: 4,
             time: 4
           },
           {
             id: 'progress3',
+            index: 3,
             totaltime: 6,
             time: 6
           },
           {
             id: 'progress4',
+            index: 4,
             totaltime: 8,
             time: 8
           }
         ],
         selectedCards: [
         ],
+        index: 4,
+        caculatingTime: 1000,
+        isCaculating: false,
+        caculateApp: null,
+        firstQueueCost: 2,
+        secondQueueCost: 4,
+        thirdQueueCost: 6,
         isDragging: false,
         delayedDragging: false
       }
@@ -93,6 +134,40 @@
         const relatedElement = relatedContext.element
         const draggedElement = draggedContext.element
         return (!relatedElement || !relatedElement.fixed) && !draggedElement.fixed
+      },
+      caculateQueue () {
+        if (this.firstQueueCards.length !== 0) {
+          this.firstQueueCards[0]['time'] -= this.firstQueueCost
+          if (this.firstQueueCards[0]['time'] > 0) {
+            this.secondQueueCards.push(this.firstQueueCards.shift())
+          } else {
+            this.firstQueueCards.shift()
+          }
+        } else if (this.secondQueueCards.length !== 0) {
+          this.secondQueueCards[0]['time'] -= this.secondQueueCost
+          if (this.secondQueueCards[0]['time'] > 0) {
+            this.thirdQueueCards.push(this.secondQueueCards.shift())
+          } else {
+            this.secondQueueCards.shift()
+          }
+        } else if (this.thirdQueueCards.length !== 0) {
+          this.thirdQueueCards[0]['time'] -= this.thirdQueueCost
+          if (this.thirdQueueCards[0]['time'] > 0) {
+            this.thirdQueueCards.push(this.thirdQueueCards.shift())
+          } else {
+            this.thirdQueueCards.shift()
+          }
+        }
+      },
+      startQueue () {
+        if (this.firstQueueCards.length === 0 && this.secondQueueCards.length === 0 && this.thirdQueueCards.length === 0) {
+          alert('请将至少一张进程卡片加入队列')
+        } else {
+          this.caculateApp = setInterval(this.caculateQueue, this.caculatingTime)
+        }
+      },
+      pauseQueue () {
+        clearInterval(this.caculateApp)
       }
     },
     computed: {
@@ -112,9 +187,35 @@
             firstCacheTotalTime += this.firstQueueCards[i]['totaltime']
             firstCacheTime += this.firstQueueCards[i]['time']
           }
-          firstCache.push({id: '...', totaltime: firstCacheTotalTime, time: firstCacheTime})
+          firstCache.push({id: '...', index: this.firstQueueCards[3]['index'], totaltime: firstCacheTotalTime, time: firstCacheTime})
         }
         return firstCache
+      },
+      secondQueueCardsCache: function () {
+        var secondCache = this.secondQueueCards.slice(0, 3)
+        if (this.secondQueueCards.length >= 4) {
+          var secondCacheTime = 0
+          var secondCacheTotalTime = 0
+          for (var i = 3; i < this.secondQueueCards.length; i++) {
+            secondCacheTotalTime += this.secondQueueCards[i]['totaltime']
+            secondCacheTime += this.secondQueueCards[i]['time']
+          }
+          secondCache.push({id: '...', index: this.secondQueueCards[3]['index'], totaltime: secondCacheTotalTime, time: secondCacheTime})
+        }
+        return secondCache
+      },
+      thirdQueueCardsCache: function () {
+        var thirdCache = this.thirdQueueCards.slice(0, 3)
+        if (this.thirdQueueCards.length >= 4) {
+          var thirdCacheTime = 0
+          var thirdCacheTotalTime = 0
+          for (var i = 3; i < this.thirdQueueCards.length; i++) {
+            thirdCacheTotalTime += this.thirdQueueCards[i]['totaltime']
+            thirdCacheTime += this.thirdQueueCards[i]['time']
+          }
+          thirdCache.push({id: '...', index: this.thirdQueueCards[3]['index'], totaltime: thirdCacheTotalTime, time: thirdCacheTime})
+        }
+        return thirdCache
       }
     },
     watch: {
@@ -134,9 +235,10 @@
       },
       waitingCards () {
         if (this.waitingCards.length < 4) {
-          var randomProcessId = 'process' + Math.ceil(Math.random() * 3)
+          var randomProcessId = 'process' + Math.ceil(Math.random() * 20)
           var randomProcessTime = Math.ceil(Math.random() * 20)
-          this.waitingCards.push({id: randomProcessId, totaltime: randomProcessTime, time: randomProcessTime})
+          ++(this.index)
+          this.waitingCards.push({id: randomProcessId, index: this.index, totaltime: randomProcessTime, time: randomProcessTime})
         }
       }
     },
@@ -173,6 +275,24 @@
     left: 160px;
     top: 390px;
   }
+  .buttonsFirstRow{
+    position: fixed;
+    width: 160px;
+    left: 815px;
+    top: 387px;
+  }
+  .buttonsSecondRow{
+    position: fixed;
+    width: 140px;
+    left: 815px;
+    top: 422px;
+  }
+  .buttonsThirdRow{
+    position: fixed;
+    width: 140px;
+    left: 815px;
+    top: 455px;
+  }
   .chooseCard{
     position: fixed;
     width: 400px;
@@ -194,14 +314,16 @@
     top: 515px;
   }
   .slide-enter-active {
-    transition: all .5s ease;
+    transition: all 1s ease-in;
   }
   .slide-leave-active {
-    transition: opacity .2s;
-    opacity: 0;
+    transition: all .5s ease-out;
   }
   .slide-enter{
     transform: translateX(10px);
+    opacity: 0;
+  }
+  .slide-leave{
     opacity: 0;
   }
   .ghost{
