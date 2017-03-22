663b88f66acf6aa0f2e3e1d0c04ff258b6e49f9b
yokin <yokinzhang@gmail.com>
2017-03-22 10:19:39 +0800

    delete the start page

--- app/src/renderer/routes.js
+++ app/src/renderer/routes.js
@@ -1,11 +1,6 @@
 export default [
   {
     path: '/',
-    name: 'start-page',
-    component: require('components/startPage')
-  },
-  {
-    path: '/mainDesk',
     name: 'main-desk',
     component: require('components/mainDesk')
   },
